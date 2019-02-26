7736b114db5f34a8d3366468830b60e0c91691c6
jiwon17ar <jiwon17ar@gmail.com>
2019-02-26 16:54:42 +0900

    Firebase 기본연동 및 푸시 테스트 성공 ver.1

---
+++ Firebase/fcm3/app/src/main/java/fcm/test/com/MyFirebaseInstanceIDService.java
@@ -0,0 +1,22 @@
+package fcm.test.com;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("IDService", "Refreshed token: " + refreshedToken);
+
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+
+    }
+}
+
+
